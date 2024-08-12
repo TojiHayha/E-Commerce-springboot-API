5efecd0232dc452ddd47116727fb2d21f445d87c
TojiHayha <fernando.aq502@gmail.com>
2024-08-12 03:21:12 -0300

    Config API

---
+++ src/test/java/com/ecommercek/ecommercek/EcommercekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommercek.ecommercek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
